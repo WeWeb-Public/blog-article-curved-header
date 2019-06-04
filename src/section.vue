5f83c2eabb6e4a7e383a816e31c1d605842ed3bd
fbriou <fbriou@gmail.com>
2019-06-04 22:14:49 +0200

    v0

--- src/section.vue
+++ src/section.vue
@@ -4,22 +4,54 @@
 
 <!-- This is your HTML -->
 <template>
-    <div class="hello">
+    <div class="ouispoon-usecase-header">
         <!-- wwManager:start -->
         <wwSectionEditMenu v-bind:sectionCtrl="sectionCtrl"></wwSectionEditMenu>
         <!-- wwManager:end -->
         <!-- Weweb Wallpaper -->
         <wwObject class="background" v-bind:ww-object="section.data.color" ww-category="background"></wwObject>
 
-        <div class="content">
-            <!-- Hello world ! -->
-            <wwObject v-bind:ww-object="section.data.helloWorld"></wwObject>
-            <wwObject class="hello-image" v-bind:ww-object="section.data.image1"></wwObject>
-
-            <!-- LIST -->
-            <wwLayoutColumn tag="div" ww-default="ww-text" :ww-list="section.data.list" class="list" @ww-add="add(section.data.list, $event)" @ww-remove="remove(section.data.list, $event)">
-                <wwObject tag="div" v-for="object in section.data.list" :key="object.uniqueId" :ww-object="object"></wwObject>
-            </wwLayoutColumn>
+        <div class="header">
+            <wwObject class="image" ww-category="background" v-bind:ww-object="section.data.header"></wwObject>
+        </div>
+        <div class="row-container">
+            <div class="round">
+                <wwObject v-bind:ww-object="section.data.roundImage"></wwObject>
+            </div>
+            <div class="left">
+                <wwObject class="show-mobile" v-bind:ww-object="section.data.btnLeftMobile"></wwObject>
+                <wwObject class="show-desktop" v-if="!isMobile" v-bind:ww-object="section.data.btnLeft"></wwObject>
+            </div>
+            <div class="right">
+                <span class="show-mobile">
+                    <wwObject v-bind:ww-object="section.data.share" @click="toggleDisplaySn()"></wwObject>
+                    <div class="icon-container" :style="{'display': displaySn}">
+                        <wwObject class="icon" v-bind:ww-object="section.data.icon1"></wwObject>
+                        <wwObject class="icon" v-bind:ww-object="section.data.icon2"></wwObject>
+                        <wwObject class="icon" v-bind:ww-object="section.data.icon3"></wwObject>
+                    </div>
+                </span>
+                <span class="show-desktop">
+                    <div class="icon">
+                        <wwObject v-bind:ww-object="section.data.icon1"></wwObject>
+                        <div class="tooltip" :class="{'force-display': editMode}">
+                            <wwObject v-bind:ww-object="section.data.icon1Tip"></wwObject>
+                        </div>
+                    </div>
+                    <div class="icon">
+                        <wwObject v-bind:ww-object="section.data.icon2"></wwObject>
+                        <div class="tooltip" :class="{'force-display': editMode}">
+                            <wwObject v-bind:ww-object="section.data.icon2Tip"></wwObject>
+                        </div>
+                    </div>
+                    <div class="icon">
+                        <wwObject v-bind:ww-object="section.data.icon3"></wwObject>
+                        <div class="tooltip" :class="{'force-display': editMode}">
+                            <wwObject v-bind:ww-object="section.data.icon3Tip"></wwObject>
+                        </div>
+                    </div>
+                </span>
+            </div>
         </div>
     </div>
 </template>
@@ -35,6 +67,7 @@ export default {
     },
     data() {
         return {
+            showSn: false
         }
     },
     computed: {
@@ -43,6 +76,16 @@ export default {
         // Use it has you like !
         section() {
             return this.sectionCtrl.get();
+        },
+        editMode() {
+            return this.sectionCtrl.getEditMode() == 'CONTENT'
+        },
+        displaySn() {
+            if (this.showSn) {
+                return 'initial'
+            } else {
+                return 'none'
+            }
         }
     },
     created() {
@@ -53,38 +96,87 @@ export default {
         this.section.data = this.section.data || {};
 
         //Initialize content
-        if (!this.section.data.helloWorld) {
-            this.section.data.helloWorld = wwLib.wwObject.getDefault({
-                type: 'ww-text',
-                data: {
-                    text: {
-                        fr_FR: 'Hello World FR !',
-                        en_GB: 'Hello World !'
-                    }
-                }
-            });
-            needUpdate = true
-        }
-
         if (!this.section.data.color) {
             this.section.data.color = wwLib.wwObject.getDefault({
                 type: 'ww-color'
             });
-            needUpdate = true
         }
 
-        if (!this.section.data.image1) {
-            this.section.data.image1 = wwLib.wwObject.getDefault({
+        if (!this.section.data.header) {
+            this.section.data.header = wwLib.wwObject.getDefault({
                 type: 'ww-image'
+            });
+        }
+
+        if (!this.section.data.roundImage) {
+            this.section.data.roundImage = wwLib.wwObject.getDefault({
+                type: 'ww-image'
+            });
+        }
+
+
+        if (!this.section.data.btnLeft) {
+            this.section.data.btnLeft = wwLib.wwObject.getDefault({
+                type: 'ww-button'
             });
             needUpdate = true
         }
 
-        if (!this.section.data.list) {
-            this.section.data.list = [];
-            needUpdate = true;
+        if (!this.section.data.btnLeftMobile) {
+            this.section.data.btnLeftMobile = wwLib.wwObject.getDefault({
+                type: 'ww-button'
+            });
+            needUpdate = true
         }
 
+        if (!this.section.data.icon1) {
+            this.section.data.icon1 = wwLib.wwObject.getDefault({
+                type: 'ww-icon'
+            });
+            needUpdate = true
+        }
+
+        if (!this.section.data.icon1Tip) {
+            this.section.data.icon1Tip = wwLib.wwObject.getDefault({
+                type: 'ww-text'
+            });
+            needUpdate = true
+        }
+
+        if (!this.section.data.icon2) {
+            this.section.data.icon2 = wwLib.wwObject.getDefault({
+                type: 'ww-icon'
+            });
+            needUpdate = true
+        }
+
+        if (!this.section.data.icon2Tip) {
+            this.section.data.icon2Tip = wwLib.wwObject.getDefault({
+                type: 'ww-text'
+            });
+            needUpdate = true
+        }
+
+        if (!this.section.data.icon3) {
+            this.section.data.icon3 = wwLib.wwObject.getDefault({
+                type: 'ww-icon'
+            });
+            needUpdate = true
+        }
+
+        if (!this.section.data.icon3Tip) {
+            this.section.data.icon3Tip = wwLib.wwObject.getDefault({
+                type: 'ww-text'
+            });
+            needUpdate = true
+        }
+
+        if (!this.section.data.share) {
+            this.section.data.share = wwLib.wwObject.getDefault({
+                type: 'ww-icon'
+            });
+            needUpdate = true
+        }
 
         if (needUpdate) {
             this.sectionCtrl.update(this.section);
@@ -92,23 +184,26 @@ export default {
 
     },
     methods: {
+        toggleDisplaySn() {
+            this.showSn = !this.showSn;
+        }
         /* wwManager:start */
-        add(list, options) {
-            try {
-                list.splice(options.index, 0, options.wwObject);
-                this.sectionCtrl.update(this.section);
-            } catch (error) {
-                wwLib.wwLog.error('ERROR : ', error);
-            }
-        },
-        remove(list, options) {
-            try {
-                list.splice(options.index, 1);
-                this.sectionCtrl.update(this.section);
-            } catch (error) {
-                wwLib.wwLog.error('ERROR : ', error);
-            }
-        },
+        // add(list, options) {
+        //     try {
+        //         list.splice(options.index, 0, options.wwObject);
+        //         this.sectionCtrl.update(this.section);
+        //     } catch (error) {
+        //         wwLib.wwLog.error('ERROR : ', error);
+        //     }
+        // },
+        // remove(list, options) {
+        //     try {
+        //         list.splice(options.index, 1);
+        //         this.sectionCtrl.update(this.section);
+        //     } catch (error) {
+        //         wwLib.wwLog.error('ERROR : ', error);
+        //     }
+        // },
         /* wwManager:end */
 
     }
@@ -119,27 +214,153 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <!-- Add lang="scss" or others to use computed CSS -->
 <style lang="scss" scoped>
-.hello {
-    padding: 100px 50px;
-}
-
-.background {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-}
-
-.content {
-    position: relative;
-    .hello-image {
-        width: 200px;
+$roundWidth: 200px;
+$margin: 100px;
+.ouispoon-usecase-header {
+    .svg {
+        width: 100%;
+        position: relative;
     }
-    .list {
-        margin-top: 20px;
-        width: 50%;
-        margin-left: 25%;
+    .background {
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        width: 100%;
+    }
+
+    .header {
+        position: relative;
+        width: 100%;
+        height: 180px;
+        border-bottom-left-radius: 50%;
+        border-bottom-right-radius: 50%;
+        clip-path: ellipse(70% 70% at 50% 30%);
+        @media (min-width: 768px) {
+            height: 250px;
+        }
+        @media (min-width: 992px) {
+            height: 300px;
+        }
+        @media (min-width: 1200px) {
+            height: 400px;
+        }
+        .image {
+            position: absolute;
+            top: 0;
+            left: 0;
+            height: 100%;
+            width: 100%;
+        }
+    }
+    .row-container {
+        position: relative;
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        padding-top: 20px;
+        margin-bottom: 100px;
+        // overflow: initial;
+        .round {
+            position: absolute;
+            left: 50%;
+            top: 0;
+            width: 100px;
+            height: 100px;
+            transform: translate(-50%, -50%);
+
+            @media (min-width: 768px) {
+                width: 150px;
+                height: 150px;
+            }
+            @media (min-width: 992px) {
+                width: 180px;
+                height: 180px;
+            }
+            @media (min-width: 1200px) {
+                width: 200px;
+                height: 200px;
+            }
+        }
+        .left,
+        .right {
+            width: calc(50% - 50px);
+            display: flex;
+            justify-content: center;
+
+            @media (min-width: 768px) {
+                width: calc(50% - 70px);
+            }
+            @media (min-width: 992px) {
+                width: calc(50% - 90px);
+            }
+            @media (min-width: 1200px) {
+                width: calc(50% - 100px);
+            }
+            .show-mobile {
+                display: flex;
+                @media (min-width: 992px) {
+                    display: none;
+                }
+            }
+
+            .show-desktop {
+                display: none;
+                @media (min-width: 992px) {
+                    display: flex;
+                }
+            }
+            .icon {
+                position: relative;
+                margin-right: 10px;
+                cursor: pointer;
+                &:hover {
+                    .tooltip {
+                        display: flex;
+                    }
+                }
+                .tooltip {
+                    position: absolute;
+                    width: max-content;
+                    display: none;
+                    padding: 5px 10px;
+                    justify-content: center;
+                    background-color: white;
+                    border-radius: 10px;
+                    top: 0px;
+                    left: 50%;
+                    transform: translate(-50%, calc(-100% - 15px));
+                    z-index: 2;
+                    flex-wrap: nowrap;
+                    filter: drop-shadow(0px 0px 10px rgba(74, 74, 74, 0.2));
+                    &:after {
+                        content: "";
+                        clip-path: polygon(50% 60%, 0 0, 100% 0);
+                        z-index: 2;
+                        position: absolute;
+                        width: 15px;
+                        height: 15px;
+                        transform: translateX(-50%);
+                        bottom: -15px;
+                        left: 50%;
+                        background-color: white;
+                    }
+                }
+                .force-display {
+                    display: flex;
+                }
+            }
+            .icon-container {
+                display: none;
+                position: absolute;
+                margin-top: 60px;
+                z-index: 2;
+                cursor: pointer;
+                .icon {
+                    margin-bottom: 10px;
+                }
+            }
+        }
     }
 }
 </style>
